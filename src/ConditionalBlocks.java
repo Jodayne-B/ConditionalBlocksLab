caf2eb8e623134e52920d93944f5815a4f9f3c41
Jodayne-B <daniyyel1017@gmail.com>
2021-06-30 22:06:47 -0400

    This is my first commit

---
+++ src/ConditionalBlocks.java
@@ -0,0 +1,13 @@
+public class ConditionalBlocks {  
+  
+ public static void main(String[] args) {  
+   boolean b = true;  //executes the if loop and then prints the statement outside the loop
+  
+   if (b){  
+  System.out.println("inside the if-statement");  
+   }  
+  
+   System.out.println("Outside of the if-statement");  
+ }  
+  
+}
