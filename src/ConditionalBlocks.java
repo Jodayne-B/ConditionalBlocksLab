17686166861263c2b60cb0b9c5ea00289bc097d0
Jodayne-B <daniyyel1017@gmail.com>
2021-06-30 22:12:23 -0400

    Made some changes to include else loops

--- src/ConditionalBlocks.java
+++ src/ConditionalBlocks.java
@@ -1,13 +1,21 @@
-public class ConditionalBlocks {  
-  
- public static void main(String[] args) {  
-   boolean b = true;  //executes the if loop and then prints the statement outside the loop
-  
-   if (b){  
-  System.out.println("inside the if-statement");  
-   }  
-  
-   System.out.println("Outside of the if-statement");  
- }  
-  
+public class ConditionalBlocks {
+
+  public static void main(String[] args) {
+    boolean firstCondition = false;
+    boolean secondCondition = true;
+
+    if (firstCondition){
+      System.out.println("inside the if-statement");
+
+      if (secondCondition){
+        System.out.println("inside the nested if-statement");
+      }
+    }else if (secondCondition){
+      System.out.println(5);
+    }else {
+      System.out.println("inside the else-statement");
+    }
+
+    System.out.println("Outside of the if-statement");
+ }
 }
